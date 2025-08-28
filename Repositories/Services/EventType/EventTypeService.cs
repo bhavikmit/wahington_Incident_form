20fc889c4acb086e382e19fc7bd6deb7638d72c9
bhavikmit <bhavik.mscit@gmail.com>
2025-08-28 11:34:17 +0530

    EventType Functionality in Settings

---
+++ Repositories/Services/EventType/EventTypeService.cs
@@ -0,0 +1,59 @@
+﻿using Models.Common.Interfaces;
+using ViewModels.Shared;
+using Models;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Centangle.Common.ResponseHelpers.Models;
+using DataLibrary;
+using Pagination;
+using System.Linq.Expressions;
+using ViewModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Repositories.Common
+{
+    public class EventTypeService<CreateViewModel, UpdateViewModel, DetailViewModel>
+        : BaseService<EventType, CreateViewModel, UpdateViewModel, DetailViewModel>,
+          IEventTypeService<CreateViewModel, UpdateViewModel, DetailViewModel>
+        where DetailViewModel : class, IBaseCrudViewModel, new()
+        where CreateViewModel : class, IBaseCrudViewModel, new()
+        where UpdateViewModel : class, IBaseCrudViewModel, IIdentitifier, new()
+    {
+        private readonly ModelStateDictionary _modelState;
+        private readonly ApplicationDbContext _db;
+
+        public EventTypeService(
+            ApplicationDbContext db,
+            ILogger<EventTypeService<CreateViewModel, UpdateViewModel, DetailViewModel>> logger,
+            IMapper mapper,
+            IRepositoryResponse response,
+            IActionContextAccessor actionContext)
+            : base(db, logger, mapper, response)
+        {
+            _modelState = actionContext.ActionContext.ModelState;
+            _db = db;
+        }
+
+        public override async Task<Expression<Func<EventType, bool>>> SetQueryFilter(IBaseSearchModel filters)
+        {
+            var searchFilters = filters as EventTypeSearchViewModel;
+
+            return x =>
+                        (
+                            (
+                                string.IsNullOrEmpty(searchFilters.Search.value)
+                                ||
+                                x.Name.ToLower().Contains(searchFilters.Search.value.ToLower())
+                                ||
+                                x.Description.ToLower().Contains(searchFilters.Search.value.ToLower())
+                            )
+                        )
+                        &&
+                        (string.IsNullOrEmpty(searchFilters.Name) || x.Name.ToLower().Contains(searchFilters.Name.ToLower()))
+                        &&
+                        (string.IsNullOrEmpty(searchFilters.Description) || x.Description.ToLower().Contains(searchFilters.Description.ToLower()))
+                        ;
+        }
+    }
+}

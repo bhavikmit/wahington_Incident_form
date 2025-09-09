261a5d45e1f1897016ea9f0fcef8222d388db05d
Vikram <tbs.vikram@gmail.com>
2025-09-09 11:27:42 +0530

    submit changes of autosearch for incident location

---
+++ Repositories/Services/ArcGis/Interface/IArcGisGeocodingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Services.ArcGis.Interface
+{
+    public interface IArcGisGeocodingService
+    {
+        Task<List<string>> GetSuggestionsAsync(string text);
+        Task<(double lat, double lon, string address)?> GetCoordinatesAsync(string magicKey);
+    }
+}

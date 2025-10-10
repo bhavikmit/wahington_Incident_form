fcedced14460d87dd22bdb5e72e579dcd00eee76
bhavikmit <bhavik.mscit@gmail.com>
2025-10-10 15:41:23 +0530

    add Material table

--- DataLibrary/ApplicationDbContext.cs
+++ DataLibrary/ApplicationDbContext.cs
@@ -108,6 +108,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
     public DbSet<Progress> Progress { get; set; }
     public DbSet<AdditionalLocations> AdditionalLocations { get; set; }
     public DbSet<EquipmentFields> EquipmentFields { get; set; }
+    public DbSet<Material> Materials { get; set; }
 
     [DbFunction("GetWeekNumber", "dbo")]
     public int GetWeekNumber(DateTime dateToCheck)
